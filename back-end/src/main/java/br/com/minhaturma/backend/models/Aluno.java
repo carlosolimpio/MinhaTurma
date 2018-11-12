6491dc08b47886131c1fc3115b36856cc519982b
merciof <mercio.andrade@ufrpe.br>
2018-11-12 11:22:44 -0300

    definição da classe Aluno como uma entidae da base de dados e definição da chave primária 'id'

--- back-end/src/main/java/br/com/minhaturma/backend/models/Aluno.java
+++ back-end/src/main/java/br/com/minhaturma/backend/models/Aluno.java
@@ -1,6 +1,17 @@
 package br.com.minhaturma.backend.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String nome;
     private String email;
     private String turma;
